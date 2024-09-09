3845b2b78095366eb0ed0a3c57372af7b21f5919
thoriqramadhan <respect.thoriq@gmail.com>
2024-09-09 15:49:43 +0700

    feat: redesign nav

--- app/ui/Sidenav.jsx
+++ app/ui/Sidenav.jsx
@@ -9,44 +9,54 @@ export default function Sidenav() {
   const path = usePathname();
   const iconSize = 30;
   console.log(path == "/");
+  const iconObj = [
+    {
+      href: "/",
+      icon: React.cloneElement(<FaHome />),
+      size: 30,
+    },
+    {
+      href: "/tag",
+      icon: React.cloneElement(<FaTag />),
+      size: 27,
+    },
+    {
+      href: "/profile",
+      icon: React.cloneElement(<FaUser />),
+      size: 26,
+    },
+  ];
   return (
-    <div
-      className="w-full h-20 bg-red-100 rounded-[35px] flex justify-evenly items-center fixed bottom-0 
-      md:static md:w-[100px] md:rounded-none md:h-full md:flex-col md:items-center md:justify-start md:gap-y-10 md:pt-10"
-    >
-      <Link href={"/"} className="block w-full">
-        <div className="md:flex md:justify-center md:relative md:py-1">
-          <FaHome className={`w-[${iconSize}px] h-[${iconSize}px]`} />
-          <span
-            className={clsx("w-1 h-full bg-sky-500 absolute right-0", {
-              block: path == "/",
-              "hidden ": path != "/",
-            })}
-          ></span>
-        </div>
-      </Link>
-      <Link href={"/tag"} className="block w-full">
-        <div className="md:flex md:justify-center md:relative md:py-1">
-          <FaTag className={`w-[${iconSize}px] h-[${iconSize}px]`} />
-          <span
-            className={clsx("w-1 h-full bg-sky-500 absolute right-0", {
-              block: path == "/tag",
-              "hidden ": path != "/tag",
-            })}
-          ></span>
-        </div>
-      </Link>
-      <Link href={"/profile"} className="block w-full">
-        <div className="md:flex md:justify-center md:relative md:py-1">
-          <FaUser className={`w-[${iconSize}px] h-[${iconSize}px]`} />
-          <span
-            className={clsx("w-1 h-full bg-sky-500 absolute right-0", {
-              block: path == "/profile",
-              "hidden ": path != "/profile",
-            })}
-          ></span>
-        </div>
-      </Link>
+    <div className="w-full flex justify-center pb-10 fixed bottom-0 md:w-[80px] md:static ">
+      <div
+        className="w-[85%] h-20 bg-white border rounded-[35px] flex justify-evenly items-center mb-5
+       md:rounded-none md:h-full md:flex-col md:items-center md:justify-start md:gap-y-10 md:pt-10 md:w-full"
+      >
+        {iconObj.map((icon) => (
+          <Link href={icon.href} className="block md:w-full" key={icon.href}>
+            <div className="relative md:flex md:w-full md:justify-center md:relative md:py-1">
+              {React.cloneElement(icon.icon, {
+                style: {
+                  width: `${icon.size}px`,
+                  height: `${icon.size}px`,
+                },
+                className: clsx("z-30 relative md:text-black", {
+                  "text-white": path === icon.href,
+                }),
+              })}
+              <span
+                className={clsx(
+                  "w-[45px] h-[45px] rounded-full -left-[7px] -top-[6px] md:w-1 md:h-full bg-sky-500 absolute md:right-0 md:left-auto md:top-auto",
+                  {
+                    block: path == icon.href,
+                    "hidden ": path != icon.href,
+                  }
+                )}
+              ></span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
