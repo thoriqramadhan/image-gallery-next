8e4c3053f2f98fe98e00137916be7a13de2cee47
thoriqramadhan <respect.thoriq@gmail.com>
2024-09-11 11:06:23 +0700

    feat: adding blur when loading on image

--- app/(overview)/page.jsx
+++ app/(overview)/page.jsx
@@ -1,25 +1,55 @@
-import { ImgCard } from "@/components/Card";
+import { ImgCard } from "@/components/client/Card";
 import { getFakeData, getImg } from "../lib/data";
 import { ImgCardSkeletons } from "@/components/skeleton/skeletons";
 import { Suspense } from "react";
+import Image from "next/image";
+import { BentoCardWrapper } from "@/components/server/Card";
+import { allBlurredDataUrls } from "../lib/utils";
 
 export default async function Home() {
+  const randomElements = (limit) => {
+    const numbers = [];
+    for (let index = 0; index < limit; index++) {
+      numbers.push(<li>{index}</li>);
+    }
+    return numbers;
+  };
+  // const element1 = randomElements(5);
+  // const element2 = randomElements(5);
+  // const element3 = randomElements(5);
+  const fakeData1 = await allBlurredDataUrls(await getFakeData(5));
+  const fakeData2 = await allBlurredDataUrls(await getFakeData(5));
+  const fakeData3 = await allBlurredDataUrls(await getFakeData(5));
+  console.log(fakeData1);
+
   return (
     <div className="w-full h-full bg-white pb-[90px] md:px-10 overflow-y-auto thin-scrollbar">
       <div className="w-full py-10">
         <h1 className="text-xl font-medium tracking-wide mb-10 mx-10">
           Members Photo
         </h1>
-        <div className="border px-7 py-14 grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-6 lg:gap-3">
+        <div className="">
+          <div className="w-full h-screen grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <div className="flex-1 flex flex-col gap-y-3">
+              <BentoCardWrapper arr={fakeData1} />
+            </div>
+            <div className="flex-1 flex flex-col gap-y-3">
+              <BentoCardWrapper arr={fakeData2} />
+            </div>
+            <div className="flex-1 flex flex-col gap-y-3">
+              <BentoCardWrapper arr={fakeData3} />
+            </div>
+          </div>
+        </div>
+        {/* <div className="border px-7 py-14 grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-6 lg:gap-3">
           <Suspense fallback={<ImgCardSkeletons />}>
             <ImgCardWrapper />
           </Suspense>
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
-
 async function ImgCardWrapper() {
   // const imgDatas = await getImg(1, 6);
   const imgDatas = await getFakeData(12);
---
+++ components/server/Card.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { getBase64, RandomNumber } from "@/app/lib/utils";
+import Link from "next/link";
+
+export function BentoCardWrapper({ arr }) {
+  return (
+    <>
+      {arr.map((item) => (
+        <BentoCardItem url={item.urls} id={item.id} key={item.id} />
+      ))}
+    </>
+  );
+}
+export async function BentoCardItem({ url, id }) {
+  const base64img = await getBase64(url);
+  return (
+    <Link className="w-full h-auto relative group" href={`/${id}/detail`}>
+      <Image
+        src={`${url.regular}`}
+        alt="Example Image"
+        width={1080} // Sesuaikan width dan height untuk menjaga rasio aspek
+        height={720}
+        layout="responsive" // Menjaga rasio aspek dengan lebar responsif
+        objectFit="cover"
+        placeholder="blur"
+        blurDataURL={url.base64}
+      />
+      <CardPreview />
+    </Link>
+  );
+}
+const CardPreview = () => {
+  return (
+    <div className="w-full h-full bg-black/45 absolute top-0 opacity-0 transition-300 group-hover:opacity-100"></div>
+  );
+};
