12b51df4da8f3107debbedbeace2670ce132283a
thoriqramadhan <respect.thoriq@gmail.com>
2024-09-19 10:21:40 +0700

    fix: change middleware config

--- middleware.js
+++ middleware.js
@@ -1,38 +1,33 @@
 import { NextResponse } from 'next/server'
 import { decrypt } from '@/app/lib/session'
 import { cookies } from 'next/headers'
- 
+
 // 1. Specify protected and public routes
-const protectedRoutes = ['/profile']
-const publicRoutes = ['/login', '/signup']
- 
+const publicRoutes = ['/login', '/register']
+
 export default async function middleware(req) {
-  // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
-  const isPublicRoute = publicRoutes.includes(path)
- 
+
+  // 2. Check if the current route is public
+  const isPublicRoute = publicRoutes.some((route) => path.startsWith(route))
+
   // 3. Decrypt the session from the cookie
   const cookie = cookies().get('session')?.value
   const session = await decrypt(cookie)
- 
-  // 5. Redirect to /login if the user is not authenticated
-  if (isProtectedRoute && !session?.userId) {
+
+  // 4. Redirect to /login if the user is not authenticated and route is protected
+  if (!isPublicRoute && !session?.userId) {
     return NextResponse.redirect(new URL('/login', req.nextUrl))
   }
- 
-  // 6. Redirect to /dashboard if the user is authenticated
-  if (
-    isPublicRoute &&
-    session?.userId &&
-    !req.nextUrl.pathname.startsWith('/profile')
-  ) {
-    return NextResponse.redirect(new URL('/profile', req.nextUrl))
+
+  // 5. Redirect to /profile if the user is authenticated and accessing a public route
+  if (isPublicRoute && session?.userId) {
+    return NextResponse.redirect(new URL('/?page=1&per_page=12', req.nextUrl))
   }
- 
+
   return NextResponse.next()
 }
- 
+
 // Routes Middleware should not run on
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
