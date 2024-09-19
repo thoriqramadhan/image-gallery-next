91992f593a92a1535a9355866730c198eaf768b5
thoriqramadhan <respect.thoriq@gmail.com>
2024-09-19 11:51:00 +0700

    fix: set exp date session

--- app/lib/action.js
+++ app/lib/action.js
@@ -132,7 +132,7 @@ export async function updateUser(formData) {
 }
 export async function logout() {
     cookies().delete('session')
-    cookies().set('flashMessage' , JSON.stringify({message: 'user logout sucessfully' , type: 'success'}) , {path:'/' })
+    cookies().set('flashMessage' , JSON.stringify({message: 'user logout sucessfully' , type: 'success'}) , {path:'/' , maxAge:10 })
     redirect('/login')
 }
 export async function getUserByEmail(email) {
